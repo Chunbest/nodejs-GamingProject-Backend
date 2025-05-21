1e28507af29afc1926774703036ba5c4f05dbed4
chun <>
2025-05-21 10:08:44 +0800

    test postsignin API

--- db/data-source.js
+++ db/data-source.js
@@ -1,23 +1,24 @@
-const { DataSource } = require('typeorm')
-const config = require('../config/index')
+const { DataSource } = require("typeorm");
+const config = require("../config/index");
 
-const Users = require('../entities/Users')
-const Products = require('../entities/Products')
-const ProductCategories = require('../entities/ProductCategories')
-const ProductTags = require('../entities/ProductTags')
-const ProductLinkTags = require('../entities/ProductLinkTags')
+const Users = require("../entities/Users");
+const Products = require("../entities/Products");
+const ProductCategories = require("../entities/ProductCategories");
+const ProductTags = require("../entities/ProductTags");
+const ProductLinkTags = require("../entities/ProductLinkTags");
+
 
 const dataSource = new DataSource({
-  type: 'postgres',
-  host: config.get('db.host'),
-  port: config.get('db.port'),
-  username: config.get('db.username'),
-  password: config.get('db.password'),
-  database: config.get('db.database'),
-  synchronize: config.get('db.synchronize'),
-  poolSize: 10,
-  entities: [Users, ProductCategories, ProductTags, Products, ProductLinkTags],
-  ssl: config.get('db.ssl')
-})
+	type: "postgres",
+	host: config.get("db.host"),
+	port: config.get("db.port"),
+	username: config.get("db.username"),
+	password: config.get("db.password"),
+	database: config.get("db.database"),
+	synchronize: config.get("db.synchronize"),
+	poolSize: 10,
+	entities: [Users, ProductCategories, ProductTags, Products, ProductLinkTags],
+	ssl: config.get("db.ssl"),
+});
 
-module.exports = { dataSource }
+module.exports = { dataSource };
