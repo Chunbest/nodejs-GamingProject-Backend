4502616fa25a20817542bd2031a1d68bf4047efe
chun <>
2025-06-10 23:12:50 +0800

    Revert "修正 statusCode again"
    This reverts commit 3778fdb83bd2a3b2547a3d6a867e7ef71bada768.

--- controllers/users.js
+++ controllers/users.js
@@ -69,7 +69,7 @@ class UsersController {
 			});
 			const savedUser = await userRepository.save(newUser);
 			logger.info("新建立的使用者ID:", savedUser.id);
-			res.status(201).json({
+			res.status(200).json({
 				message: "註冊成功",
 			});
 		} catch (error) {
